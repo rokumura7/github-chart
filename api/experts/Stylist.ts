2127cd89a1710e67eaf01bd654a7c7caa3272877
rokumura7 <rokumura.8807@gmail.com>
2021-04-11 11:48:15 +0900

    refactor stylist

--- api/experts/Stylist.ts
+++ api/experts/Stylist.ts
@@ -9,45 +9,22 @@ export default class Stylist implements Expert<ResultByStylist> {
   work(props?: Props): ResultByStylist {
     const style = `
 <style>
-  .this_year {
-    stroke: #ACA
-  }
-  .last_year {
-    stroke: #CAA;
-  }
-  .card {
-    fill: #141321;
-    stroke: #E4E2E2;
-    stroke-opacity: 1;
-  }
-  .xy-axis {
-    stroke: #CCC;
-  }
-  .avg-line {
-    opacity: 0.5;
-  }
-  .ruled-line {
-    stroke: #777;
-    opacity: 0.1;
-  }
-  .months {
-    fill: #CCC;
-  }
-  .circle-points {
-    fill: #CFC;
-  }
-  line {
-    stroke-width: 2;
-  }
-  polyline {
-    fill: none;
-    stroke-width: 2;
-  }
-  polyline.this_year {
-    stroke-width: 4;
-  }
-</style>
-`
+  ${ this.makeFundamental() }
+  .this_year { stroke: #ACA; }
+  .last_year { stroke: #CAA; }
+  .card { fill: #141321; stroke: #E4E2E2; stroke-opacity: 1; }
+  .xy-axis { stroke: #CCC; }
+  .avg-line { opacity: 0.5; }
+  .ruled-line { stroke: #777; opacity: 0.1; }
+  .months { fill: #CCC; }
+  .circle-points { fill: #CFC; }
+</style>`
     return { style }
   }
+
+  private makeFundamental = () => `
+  line { stroke-width: 2; }
+  polyline { fill: none; stroke-width: 2; }
+  polyline.this_year { stroke-width: 4; }
+  `
 }
