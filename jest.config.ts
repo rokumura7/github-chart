22b65220e80bba132624ee443311289a4d097746
rokumura7 <rokumura.8807@gmail.com>
2021-05-04 20:00:00 +0900

    added coverage path ignore patterns

--- jest.config.ts
+++ jest.config.ts
@@ -31,9 +31,11 @@ export default {
   coverageDirectory: "coverage",
 
   // An array of regexp pattern strings used to skip coverage collection
-  // coveragePathIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
+  coveragePathIgnorePatterns: [
+    "/node_modules/",
+    "/api/index.ts",
+    "/lib/experts/Expert.ts"
+  ],
 
   // Indicates which provider should be used to instrument code for coverage
   coverageProvider: "v8",
